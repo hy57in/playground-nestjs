36595e51dd99c04a0b30c997e5c8649b208c5631
hy57in <gwsyl22@cau.ac.kr>
2022-11-04 03:27:14 +0900

    feat: logger 설정

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
+  const logger = new Logger();
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+  const port = 4000;
+  await app.listen(port);
+  logger.log(`Application running on port ${port}`);
 }
 bootstrap();
