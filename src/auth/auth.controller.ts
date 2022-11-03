ee16fb603e5ea5be5da669fd41d80cf0a1ce4cfb
hy57in <gwsyl22@cau.ac.kr>
2022-11-04 00:55:27 +0900

    feat: validation 추가

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, ValidationPipe } from '@nestjs/common';
 import { AuthService } from 'src/auth/auth.service';
+import { AuthCredentialsDto } from 'src/auth/dto/auth-credentials.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
+
+  @Post('/signup')
+  signUp(
+    @Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto,
+  ): Promise<void> {
+    return this.authService.signUp(authCredentialsDto);
+  }
 }
--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,4 +1,12 @@
-import { Repository } from 'typeorm';
+import { EntityRepository, Repository } from 'typeorm';
 import { User } from 'src/auth/user.entity';
+import { AuthCredentialsDto } from 'src/auth/dto/auth-credentials.dto';
 
-export class UserRepository extends Repository<User> {}
+@EntityRepository(User)
+export class UserRepository extends Repository<User> {
+  async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    const { username, password } = authCredentialsDto;
+    const user = this.create({ username, password });
+    await this.save(user);
+  }
+}
