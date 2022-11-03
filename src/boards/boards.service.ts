c2e46eaa03f5332c29a961ff4717d29ee6c3ba6f
hy57in <gwsyl22@cau.ac.kr>
2022-11-04 03:22:20 +0900

    feat: 자신이 생성한 게시물 삭제하기

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -37,8 +37,8 @@ export class BoardsService {
     return this.boardRepository.createBoard(createBoardDto, user);
   }
 
-  async deleteBoard(id: number): Promise<void> {
-    const result = await this.boardRepository.delete(id);
+  async deleteBoard(id: number, user: User): Promise<void> {
+    const result = await this.boardRepository.delete({ id, user });
 
     if (result.affected === 0) {
       throw new NotFoundException(`Can't find Board with id ${id}`);
