e804655cc4118532c856ed39643276d542715f09
hy57in <gwsyl22@cau.ac.kr>
2022-11-04 01:12:57 +0900

    feat: 중복 닉네임 검사

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,12 +1,25 @@
 import { EntityRepository, Repository } from 'typeorm';
 import { User } from 'src/auth/user.entity';
 import { AuthCredentialsDto } from 'src/auth/dto/auth-credentials.dto';
+import {
+  ConflictException,
+  InternalServerErrorException,
+} from '@nestjs/common';
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
   async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     const { username, password } = authCredentialsDto;
     const user = this.create({ username, password });
-    await this.save(user);
+
+    try {
+      await this.save(user);
+    } catch (error) {
+      if (error.code === 23505) {
+        throw new ConflictException('Existing username');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
   }
 }
